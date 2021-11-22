db454642484f94bb72dcedf2148ef694907f734f
David Vega <ddvega@Davids-MacBook-Pro.local>
2021-11-21 18:11:46 -0600

    changed tmdb entity to static class

--- src/main/java/com/fjapi/flickjunkies/entity/Tmdb.java
+++ src/main/java/com/fjapi/flickjunkies/entity/Tmdb.java
@@ -10,60 +10,43 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Tmdb
+final public class Tmdb
 {
-    private final String apiKey;
-
-    public String getActorId(String searchVal) throws IOException
+    public static String getActorId(String searchVal, String apiKey) throws IOException
     {
         String query = "https://api.themoviedb.org/3/search/person?api_key=" + apiKey;
-        query += "&language=en-US&query=";
-        query += searchVal;
-        query += "&page=1";
-        query += "&include_adult=false";
-
+        query += "&language=en-US&query=" + searchVal + "&page=1" + "&include_adult=false";
         JSONObject result = queryApi(query);
         JSONArray res = (JSONArray) result.get("results");
         JSONObject results = (JSONObject) res.get(0);
         return results.get("id").toString();
     }
 
-    public List<Movie> movieDiscover(Discover searchData) throws IOException
+    public static List<Movie> movieDiscover(Discover searchData, String apiKey) throws IOException
     {
-        String query =
-                "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey + "&page=" + searchData.getPage();
-
-
         if (searchData.getTitle() != null)
         {
-            query = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&page=" + searchData.getPage();
-            query += "&query=" + searchData.getTitle();
-        }
-        else
-        {
-            query += "&sort_by=popularity.desc&include_adult=false&include_video=false";
-
-            if (searchData.getRatingMIN() != null) query += "&vote_average.gte=" + searchData.getRatingMIN();
-
-            if (searchData.getRatingMAX() != null) query += "&vote_average.lte=" + searchData.getRatingMAX();
-
-            if (searchData.getActorId() != null) query += "&with_people=" + searchData.getActorId();
-
-            if (searchData.getDateMIN() != null) query += "&primary_release_date.gte=" + searchData.getDateMIN();
-
-            if (searchData.getDateMAX() != null) query += "&primary_release_date.lte=" + searchData.getDateMAX();
-
-            if (searchData.getGenre() != null) query += "&with_genres=" + searchData.getGenre();
-
-            if (searchData.getLanguage() != null) query += "&with_original_language=" + searchData.getLanguage();
+            String query = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey;
+            query += "&page=" + searchData.getPage() + "&query=" + searchData.getTitle();
+            return buildMovieList(query);
         }
 
+        String query = "https://api.themoviedb.org/3/discover/movie?api_key=" + apiKey;
+        query += "&page=" + searchData.getPage() + "&sort_by=popularity.desc&include_adult=false&include_video=false";
+        if (searchData.getRatingMIN() != null) query += "&vote_average.gte=" + searchData.getRatingMIN();
+        if (searchData.getRatingMAX() != null) query += "&vote_average.lte=" + searchData.getRatingMAX();
+        if (searchData.getActorId() != null) query += "&with_people=" + searchData.getActorId();
+        if (searchData.getDateMIN() != null) query += "&primary_release_date.gte=" + searchData.getDateMIN();
+        if (searchData.getDateMAX() != null) query += "&primary_release_date.lte=" + searchData.getDateMAX();
+        if (searchData.getGenre() != null) query += "&with_genres=" + searchData.getGenre();
+        if (searchData.getLanguage() != null) query += "&with_original_language=" + searchData.getLanguage();
         return buildMovieList(query);
     }
 
     @NotNull
-    private List<Movie> buildMovieList(String query) throws IOException
+    private static List<Movie> buildMovieList(String query) throws IOException
     {
         JSONObject result = queryApi(query);
         JSONArray res = (JSONArray) result.get("results");
@@ -83,50 +66,41 @@ public class Tmdb
             movie.setBackdrop_path(parseStringFromJsonObject(obj, "backdrop_path"));
             movie.setRelease_date(parseStringFromJsonObject(obj, "release_date"));
             movie.setVote_count(parseLongFromJsonObject(obj, "vote_count"));
-            movie.setGenre_ids(GenreMap.buildGenreList(parseJsonArrayJsonObject(obj, "genre_ids")));
+            movie.setGenre_ids(GenreMap.buildGenreList(parseGenresJsonObject(obj)));
             movieList.add(movie);
 
         }
         return movieList;
     }
 
-    private Long parseLongFromJsonObject(JSONObject obj, String key)
-    {
-        if (obj.isNull(key)) return null;
-
-        return obj.getLong(key);
-    }
-
-    private Double parseDoubleFromJsonObject(JSONObject obj, String key)
-    {
-        if (obj.isNull(key)) return null;
-        return obj.getDouble(key);
-    }
-
-    private String parseStringFromJsonObject(JSONObject obj, String key)
-    {
-
-        if (obj.isNull(key)) return null;
-        return obj.getString(key);
-    }
-
-    private JSONArray parseJsonArrayJsonObject(JSONObject obj, String key)
-    {
-        if (obj.isNull(key)) return new JSONArray();
-        return obj.getJSONArray(key);
-    }
-
-    private JSONObject queryApi(String queryString) throws IOException
+    private static JSONObject queryApi(String queryString) throws IOException
     {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(queryString).build();
         Response response = client.newCall(request).execute();
-        String jsonData = response.body().string();
+        String jsonData = Objects.requireNonNull(response.body()).string();
         return new JSONObject(jsonData);
     }
 
-    public Tmdb(String apiKey)
+    private static Long parseLongFromJsonObject(JSONObject obj, String key)
     {
-        this.apiKey = apiKey;
+        return obj.isNull(key) ? null : obj.getLong(key);
     }
+
+    private static Double parseDoubleFromJsonObject(JSONObject obj, String key)
+    {
+        return obj.isNull(key) ? null : obj.getDouble(key);
+    }
+
+    private static String parseStringFromJsonObject(JSONObject obj, String key)
+    {
+        return obj.isNull(key) ? null : obj.getString(key);
+    }
+
+    private static JSONArray parseGenresJsonObject(JSONObject obj)
+    {
+        return obj.isNull("genre_ids") ? new JSONArray() : obj.getJSONArray("genre_ids");
+    }
+
+
 }
--- src/main/java/com/fjapi/flickjunkies/service/MovieService.java
+++ src/main/java/com/fjapi/flickjunkies/service/MovieService.java
@@ -1,6 +1,5 @@
 package com.fjapi.flickjunkies.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fjapi.flickjunkies.entity.Discover;
 import com.fjapi.flickjunkies.entity.Genre;
 import com.fjapi.flickjunkies.entity.Movie;
@@ -60,55 +59,39 @@ public class MovieService
 
     public List<Movie> searchMovies(Map<String, Object> payload) throws IOException
     {
-        Tmdb tmdbApi = new Tmdb(getApiKey);
         Discover searchData = new Discover();
 
         if (payload.containsKey("page"))
             searchData.setPage(payload.get("page").toString());
 
-
         if (payload.containsKey("title"))
         {
             searchData.setTitle(payload.get("title").toString());
-            return tmdbApi.movieDiscover(searchData);
+            return Tmdb.movieDiscover(searchData, getApiKey);
         }
 
         if (payload.containsKey("genre"))
             searchData.setGenre(payload.get("genre").toString());
 
         if (payload.containsKey("actor"))
-        {
-            String actorID = tmdbApi.getActorId(payload.get("actor").toString());
-            searchData.setActorId(actorID);
-        }
+            searchData.setActorId(Tmdb.getActorId(payload.get("actor").toString(), getApiKey));
 
         if (payload.containsKey("release_date_min"))
-        {
             searchData.setDateMIN(payload.get("release_date_min").toString());
-        }
 
         if (payload.containsKey("release_date_max"))
-        {
             searchData.setDateMAX(payload.get("release_date_max").toString());
-        }
 
         if (payload.containsKey("rating_min"))
-        {
             searchData.setRatingMIN(payload.get("rating_min").toString());
-        }
 
         if (payload.containsKey("rating_max"))
-        {
             searchData.setRatingMAX(payload.get("rating_max").toString());
-        }
 
         if (payload.containsKey("language"))
-        {
             searchData.setLanguage(payload.get("language").toString());
-        }
 
-
-        return tmdbApi.movieDiscover(searchData);
+        return Tmdb.movieDiscover(searchData, getApiKey);
     }
 }
 

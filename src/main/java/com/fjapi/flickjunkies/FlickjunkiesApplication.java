08e83808335b224ff233caec351c02a1a33fb3d4
David Vega <daviddvega@gmail.com>
2021-12-13 20:04:07 -0600

    changed package names.

--- src/main/java/com/fjapi/flickjunkies/FlickjunkiesApplication.java
+++ src/main/java/com/fjapi/flickjunkies/FlickjunkiesApplication.java
@@ -2,12 +2,13 @@ package com.fjapi.flickjunkies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class FlickjunkiesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FlickjunkiesApplication.class, args);
-	}
-
+public class FlickjunkiesApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FlickjunkiesApplication.class, args);
+    }
 }

934c5b3740ede5b482d6145ecea31ce56195e37f
David Vega <daviddvega@gmail.com>
2023-04-03 00:12:58 -0500

    Commit added by devtool

--- src/test/java/com/fjapi/flickjunkies/repository/MovieRepositoryTest.java
+++ src/test/java/com/fjapi/flickjunkies/repository/MovieRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
+# this is just a test
 @SpringBootTest
 class MovieRepositoryTest
 {
